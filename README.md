14d191cdc50818a3a05d28705c099f710ee38428
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-10-06 21:20:39 +0700

    feat: script js

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # RIZKY-JS1
+
+Muhammad Rizky Haksono_Afternoon_Web B
+
+Ini adalah repository untuk tugas [Infinite Learning](https://www.infinitelearning.id/) dengan ketentuan berikut:
+
+- ✨ if,else,nested if
+- ✨ switch case
+- ✨ for statement
+- ✨ while, do while
+- ✨ function
---
+++ script.js
@@ -0,0 +1,81 @@
+/**
+ * @author Rizky Haksono
+ * @Kelas Web B Afternoon
+ */
+
+// ✨ if,else,nested ifs
+function if_else(jam) {
+  if (jam < 18) {
+    salam = "Selamat Pagi";
+    console.log(salam);
+  } else {
+    salam = "Selamat Malam";
+  }
+}
+
+// ✨ switch case
+function switch_case(day) {
+  switch (day) {
+    case "Minggu":
+      console.log(day);
+      break;
+    case "Senin":
+      console.log(day);
+      break;
+    case "Selasa":
+      console.log(day);
+      break;
+    case "Rabu":
+      console.log(day);
+      break;
+    case "Kamis":
+      console.log(day);
+      break;
+    case "Jumat":
+      console.log(day);
+      break;
+    case "Sabtu":
+      console.log(day);
+      break;
+  }
+}
+
+// ✨ for statement
+function for_statement() {
+  const bandKPOP = ["NCT 127", "Twice", "NCT Dream", "Way V", "BTS", "Aespa", "Red Velvet", "Black Pink", "Seventeen", "Xodiac", "EXO", "Rizze", "New Jeans", "IVE"];
+
+  console.log("Total band KPOP: " + bandKPOP.length);
+  for (let i = 0; i < bandKPOP.length; i++) {
+    console.log(bandKPOP[i]);
+  }
+}
+
+// ✨ while, do while
+function do_while() {
+  let text = "";
+  let i = 0;
+
+  do {
+    i++;
+    console.log((text += "Bilangan ke " + i));
+  } while (i < 10);
+}
+
+// ✨ function
+function while_statement() {
+  while (i < 10) {
+    i++;
+    console.log("Bilangan ke" + i);
+  }
+}
+
+// if else and nested if else
+console.log(if_else(10));
+// switch case
+console.log(switch_case("Minggu"));
+// for
+console.log(for_statement());
+// do while
+console.log(do_while());
+// while
+console.log(while_statement());
